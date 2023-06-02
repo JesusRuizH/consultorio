d4b0d3da33e06dc433f514c7dba64c28c56a3c19
Jesus Ruiz <jesus.ruiz0216@alumnos.udg.mx>
2023-06-01 19:55:18 -0600

    ruta

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const port = 3000;
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "/public/index.html"));
 })
+
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}`);
+});
